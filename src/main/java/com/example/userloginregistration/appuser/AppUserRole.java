c8d94688e1329ca53c0f55da9dd5791502253bf3
AlexoAnimaIgnis <ballester.alexander@gmail.com>
2024-08-22 01:04:52 +0800

    added app user package and class

---
+++ src/main/java/com/example/userloginregistration/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.userloginregistration.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ src/main/java/com/example/userloginregistration/appuser/AppUserService.java
@@ -0,0 +1,20 @@
+package com.example.userloginregistration.appuser;
+
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository appUserRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return appUserRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User with email not " + email + " found"));
+    }
+}
